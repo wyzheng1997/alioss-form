8915287f272d7411ebbb8ab997a40bde199b2685
王园正 <wangyuanzheng@jinyiyun.com.cn>
2019-01-05 20:40:13 +0800

    修复关于images默认值BUG

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -3,9 +3,12 @@
     <div class="{{$viewClass['field']}}">
         @include('admin::form.error')
         <button type="button" class="btn btn-success" style="cursor: pointer;" id="{{$column}}_upload" data-warp='#{{$column}}_upload_warp'>上传图片</button>
-        @if(old($column, $value))
+        @if(($column_val = old($column, $value))
+            <?php
+                $column_val = is_array($column_val) ? $column_val : explode(',', $column_val);
+            ?>
             <div class="upload_warp" id="{{$column}}_upload_warp" style="opacity: 1; display: block;">
-                @foreach(old($column, $value) as $p)
+                @foreach($column_val as $p)
                 <div class="upload_item">
                     <span class="upload_del_btn" data-filename="{{$p}}" onclick="del_pic(this,true,'undefined')">删除</span>
                     <img src="{{$oss_url}}/{{$p}}?x-oss-process=image/resize,m_fill,w_100,h_100">
