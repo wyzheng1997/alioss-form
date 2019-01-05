4467b14c1d80dfac4ddcc4867e301b1d5a6c8fc1
JasonWang <31637036+airan587@users.noreply.github.com>
2019-01-05 20:58:14 +0800

    Update image.blade.php

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -13,7 +13,7 @@
                 <div class="upload_item">
                     <span class="upload_del_btn" data-filename="{{$p}}" onclick="del_pic(this,true,'undefined')">删除</span>
                     <img src="{{$oss_url}}/{{$p}}?x-oss-process=image/resize,m_fill,w_100,h_100">
-                    <input type="hidden" class="Js_upload_input" name="{{$column}}[]" value="{{$P}}">
+                    <input type="hidden" class="Js_upload_input" name="{{$column}}[]" value="{{$p}}">
                 </div>
                 @endforeach
             </div>
